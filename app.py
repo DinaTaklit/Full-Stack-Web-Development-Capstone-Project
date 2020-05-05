49c25bc4dba0e9d9d6a89aec91f8db18a4e96a75
DinaTaklit <ez_taklit@esi.dz>
2020-05-05 23:17:48 +0100

    fix: app.py use run without host and port to fix soket error
        - socket.error: [Errno 10013] An attempt was made to access a socket in a way forbidden by its access permissions

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ def create_app(test_config=None):
     app = Flask(__name__)
     CORS(app)
 
-    return appclear
+    return app
 
 
 APP = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    #APP.run(host='0.0.0.0', port=8080, debug=True)
+    APP.run(debug=True)
