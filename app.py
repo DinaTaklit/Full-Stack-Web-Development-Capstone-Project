23c51b88e4082fd226c521ddae01c71dee71ea6f
DinaTaklit <ez_taklit@esi.dz>
2020-05-05 23:50:22 +0100

    feat: define / endpoint

--- app.py
+++ app.py
@@ -3,10 +3,37 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+from database.models import db_drop_and_create_all, setup_db, Movie, Actor
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
+    setup_db(app)
     CORS(app)
+
+    '''
+    @TODO uncomment the following line to initialize the datbase
+    !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
+    !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
+    '''
+    db_drop_and_create_all()
+
+    # ROUTES
+    '''
+    @Done implement endpoint
+        /
+            it should be a public endpoint
+        returns status code 200 and json {"success": True, "message": "hello world"}
+        or appropriate status code indicating reason for failure
+    '''
+
+    @app.route('/')
+    def home():
+        return jsonify({
+            'success': True,
+            'message': 'hello world'
+        })
+
 
     return app
 
