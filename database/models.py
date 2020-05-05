6c13df8f3fff27c7f2b305c280e686816e826336
DinaTaklit <ez_taklit@esi.dz>
2020-05-05 21:14:31 +0100

    feat: db=> Implement movie model

--- database/models.py
+++ database/models.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
+import datetime
 
 database_filename = "database.db"
 project_dir = os.path.dirname(os.path.abspath(__file__))
@@ -28,3 +29,70 @@ db_drop_and_create_all()
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
+
+'''
+Movie
+a persistent movie entity, extends the base SQLAlchemy Model
+'''
+class Movie(db.model):
+    __tablename__ = "movie"
+    # Autoincrementing, unique primary key
+    id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
+    # String Title
+    title = Column(String(80), nullable=false)
+    # Release date
+    release_date = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
+    
+    '''
+    get_movie(self)
+        json form representation of the Movie model
+    '''
+    
+    def get_movie(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "realse_date": self.realse_date
+        }
+
+        '''
+    insert()
+        inserts a new model into a database
+        the model must have a unique name
+        the model must have a unique id or null id
+        EXAMPLE
+            movie = Movie(title=req_title, realse_date=req_realse_date)
+            movie.insert()
+    '''
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+        
+        
+    '''
+    delete()
+        deletes a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            movie = Movie(title=req_title,realse_date=req_realse_date)
+            movie.delete()
+    '''
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        
+    
+    '''
+    update()
+        updates a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            movie = Movie.query.filter(Drink.id == id).one_or_none()
+            movie.title = 'New Movie'
+            movie.update()
+    '''
+    def update(self):
+        db.session.commit()
+
+    def __repr__(self):
+        return json.dumps(self.get_movie())
