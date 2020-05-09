07c6881aeaf46b8209477419aba0f12cb1043798
DinaTaklit <ez_taklit@esi.dz>
2020-05-09 20:34:12 +0100

    feat: auth.py=> implement @requires_auth(permission) decorator method

--- auth/auth.py
+++ auth/auth.py
@@ -147,3 +147,27 @@ def verify_decode_jwt(token):
                 'description': 'Unable to find the appropriate key.'
             }, 400)
 
+'''
+@Done implement @requires_auth(permission) decorator method
+    @INPUTS
+        permission: string permission (i.e. 'post:drink')
+    it should use the get_token_auth_header method to get the token
+    it should use the verify_decode_jwt method to decode the jwt
+    it should use the check_permissions method validate claims and check the requested permission
+    return the decorator which passes the decoded payload to the decorated method
+'''
+
+
+def requires_auth(permission=''):
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            try:
+                payload = verify_decode_jwt(token)
+            except:
+                abort(401)
+            check_permissions(permission, payload)
+            return f(payload, *args, **kwargs)  
+        return wrapper
+    return requires_auth_decorator
