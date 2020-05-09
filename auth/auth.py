df5f83a43d5d4f7e2555c1e9fdb660b9e2d55b7a
DinaTaklit <ez_taklit@esi.dz>
2020-05-09 20:28:00 +0100

    feat: auth.py => implement get_token_auth_header() method

--- auth/auth.py
+++ auth/auth.py
@@ -17,3 +17,42 @@ class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
+
+## Auth Header
+
+'''
+@Done implement get_token_auth_header() method
+    it should attempt to get the header from the request
+        it should raise an AuthError if no header is present
+    it should attempt to split bearer and the token
+        it should raise an AuthError if the header is malformed
+    return the token part of the header
+'''
+def get_token_auth_header():
+    auth = request.headers.get('Authorization', None) # get the authorization part of the request headers
+    if not auth: # the header is missing
+        raise AuthError({
+            'code': 'authorization_header_missing',
+            'description': 'Authorization header is expected.'
+        }, 401)
+    parts = auth.split() # the header must contain the bearer + token
+    if parts[0].lower() != 'bearer': 
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must start with "Bearer".'
+        }, 401)
+
+    elif len(parts) == 1:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Token not found.'
+        }, 401)
+
+    elif len(parts) > 2:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must be bearer token.'
+        }, 401)
+
+    token = parts[1]
+    return token
