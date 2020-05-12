207059f699e671cdf689149f1085af98aa8d8284
DinaTaklit <ez_taklit@esi.dz>
2020-05-12 10:03:10 +0100

    refact: models=> add an argument to setup databse

--- database/models.py
+++ database/models.py
@@ -14,7 +14,7 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
-def setup_db(app):
+def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
