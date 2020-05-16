9abc0fa209cca5f2fbf5fa1ac38ed22a3a9bd94d
DinaTaklit <ez_taklit@esi.dz>
2020-05-16 06:48:37 +0100

    test-fix:get env vars from setup.sh

--- test_app.py
+++ test_app.py
@@ -7,16 +7,14 @@ from database.models import db, db_drop_and_create_all, setup_db, Movie, Actor
 from auth.auth import AuthError, requires_auth
 from sqlalchemy import Column, String, Integer, DateTime
 import logging
-from setup import CASTING_ASSISTANT_JWT, CASTING_DIRECTOR_JWT, EXECUTIVE_PRODUCER_JWT
 # define the global vars 
 database_name = "capstone_test.db"
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_path = "sqlite:///{}".format(os.path.join(project_dir, database_name))
 
-
-casting_assistant_token = CASTING_ASSISTANT_JWT
-casting_director_token = CASTING_DIRECTOR_JWT
-executive_producer_token = EXECUTIVE_PRODUCER_JWT
+casting_assistant_token = os.getenv('CASTING_ASSISTANT_JWT')
+casting_director_token = os.getenv('CASTING_DIRECTOR_JWT')
+executive_producer_token  = os.getenv('EXECUTIVE_PRODUCER_JWT')
 
 # define set authetification method 
 def setup_auth(role):
