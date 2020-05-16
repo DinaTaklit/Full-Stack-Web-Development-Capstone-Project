c02cf99f3d51c736019fe5528c3e88233008b603
DinaTaklit <ez_taklit@esi.dz>
2020-05-16 08:02:52 +0100

    style: models.py=> fix the style

--- database/models.py
+++ database/models.py
@@ -6,7 +6,8 @@ import datetime
 
 database_filename = "database.db"
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+database_path = "sqlite:///{}".format(
+    os.path.join(project_dir, database_filename))
 
 db = SQLAlchemy()
 
@@ -14,26 +15,35 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
 
+
 '''
 db_drop_and_create_all()
     drops the database tables and starts fresh
     can be used to initialize a clean database
-    !!NOTE you can change the database_filename variable to have multiple verisons of a database
+    !!NOTE you can change the database_filename
+    variable to have multiple verisons of a database
 '''
+
+
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
+
 
 '''
 Movie
 a persistent movie entity, extends the base SQLAlchemy Model
 '''
+
+
 class Movie(db.Model):
     __tablename__ = "movie"
     # Autoincrementing, unique primary key
@@ -41,15 +51,16 @@ class Movie(db.Model):
     # String Title
     title = Column(String(80), nullable=False)
     # Release date
-    release_date = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
+    release_date = Column(DateTime, nullable=False,
+                          default=datetime.datetime.utcnow)
     # Define parent-child relationship betw the Movie and Actor
     actors = db.relationship("Actor", backref="movie")
-    
+
     '''
     get_movie(self)
         json form representation of the Movie model
     '''
-    
+
     def get_movie(self):
         return {
             "id": self.id,
@@ -66,11 +77,11 @@ class Movie(db.Model):
             movie = Movie(title=req_title, realse_date=req_realse_date)
             movie.insert()
     '''
+
     def insert(self):
         db.session.add(self)
         db.session.commit()
-        
-        
+
     '''
     delete()
         deletes a new model into a database
@@ -79,11 +90,11 @@ class Movie(db.Model):
             movie = Movie(title=req_title,realse_date=req_realse_date)
             movie.delete()
     '''
+
     def delete(self):
         db.session.delete(self)
         db.session.commit()
-        
-    
+
     '''
     update()
         updates a new model into a database
@@ -93,6 +104,7 @@ class Movie(db.Model):
             movie.title = 'New Movie'
             movie.update()
     '''
+
     def update(self):
         db.session.commit()
 
@@ -105,6 +117,7 @@ Actor
 a persistent actor entity, extends the base SQLAlchemy Model
 '''
 
+
 class Actor(db.Model):
     __tablename__ = "actor"
     # Autoincrementing, unique primary key
@@ -113,16 +126,16 @@ class Actor(db.Model):
     name = Column(String(80), nullable=False)
     # Integer age
     age = Column(Integer(), nullable=False)
-    # String gender 
+    # String gender
     gender = Column(String(80), nullable=False)
-    # Add movie as foreight key for actor model 
+    # Add movie as foreight key for actor model
     movie_id = Column(Integer(), db.ForeignKey("movie.id"))
-    
+
     '''
     get_actor(self)
         json form representation of the Actor model
     '''
-    
+
     def get_actor(self):
         return {
             'id': self.id,
@@ -130,7 +143,7 @@ class Actor(db.Model):
             'age': self.age,
             'gender': self.gender
         }
-        
+
     '''
     insert()
         inserts a new model into a database
